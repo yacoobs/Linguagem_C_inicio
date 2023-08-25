3a42d835b69ce385f5c718a63bf347e98dbbb5b9
yacoobs <142899663+yacoobs@users.noreply.github.com>
2023-08-25 13:31:18 +0000

    .

--- lingc/slide/desafio.c
+++ lingc/slide/desafio.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int  bovinosFinal, bovinosComeco = 10;
+    int  bovinosFinal, bovinosDurante, bovinosComeco = 10;
     printf("Informe a quantidade final de bovinos:\n");
     scanf("%d", &bovinosFinal);
-   
+    bovinosdurante = bovinosComeco - bovinosComeco / 4 + bovinosComeco / 3
+    while(bovinosDurante <= bovinosFinal);
+    
 
+    }
+    
 
-}
+    
